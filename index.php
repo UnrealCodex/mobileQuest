e4da8910191018010c1ecb9942f9507b032ab7a7
UnrealCodex <r.alan.rdz@unrealcodex.com>
2018-04-26 12:00:42 -0500

    tet
    carga inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+<div class="container">
+¿Cuantos focos hay en su casa?...
+</div>
+</body>
+</html>
